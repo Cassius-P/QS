3e1c4ca5a15025250cd09dda850f1370202d149a
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-05-18 15:49:32 +0200

    Configuring particles and adding DIVs elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
-particlesJS.load('main', 'assets/particles.json', function() {
-    console.log('callback - particles.js config loaded');
-});
+window.onload = function () {
+    getHeight();
+};
+
+function getHeight(){
+    var nb = document.querySelectorAll(".fullScreen").length;
+    loadParticles(nb);
+}
+
+function loadParticles(nb){
+    console.log(nb);
+    document.querySelector("#bg").style.height = nb * 100 + "vh";
+
+    particlesJS.load('bg', 'assets/particles.json', function() {
+        console.log('callback - particles.js config loaded');
+    });
+}
