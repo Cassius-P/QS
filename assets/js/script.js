b57aca520056a1ede19e47f64c17c6ed784fee97
Cassius-P <49432067+Cassius-P@users.noreply.github.com>
2020-05-19 17:36:35 +0200

    Title scroll update

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,11 @@
 
 window.onload = function () {
     //createCursor();
-    //getHeight();
     loadParticles();
 
 };
 
-window.onresize = function () {
-    //getHeight();
-};
+
 
 function createCursor(){
     new kursor({
@@ -17,11 +14,6 @@ function createCursor(){
         removeDefaultCursor: true,
         color: "#FFF"
     })
-}
-
-function getHeight(){
-    var elmtHeight = document.querySelector("#main").offsetHeight;
-    document.querySelector("#main").style.height = elmtHeight + "px";
 }
 
 function loadParticles(){
@@ -32,19 +24,54 @@ function loadParticles(){
 
 
 
-
+/* ===== Title Scroling ===== */
 $('#main').scroll( function() {
+    var partsName = document.querySelectorAll("div[data-title]");
+    var titles = [];
+    for (let i = 0; i < partsName.length ; i++) {
+        var name = partsName[i].getAttribute("data-title");
+        titles.push(name);
+    }
 
     var parts = document.querySelector("#parts");
+    var title = parts.querySelector('h2');
     var scrolled = this.scrollTop;
+    title.innerHTML = titles[0];
 
-    if ( scrolled >= 1000) {
-
-        console.log(scrolled+"YES");
-        var scrollFrom = (scrolled - 1000)*5 ;
+    if ( scrolled >= 1000 && scrolled < 2000) {
+        var scrollFrom = (scrolled - 1000)*4 ;
         parts.scroll(scrollFrom, 0);
 
-    } else {
-        console.log(scrolled);
+
+    } else if ( scrolled >= 2000){
+        title.innerHTML = titles[1];
+        parts.scrollLeft;
+        var scrollFrom = (scrolled - 2000)*1.5 ;
+        parts.scroll(scrollFrom, 0);
+        console.log("2000")
     }
+    console.log(scrolled);
 });
+
+
+/* ===== Team on hover ===== */
+var team = document.querySelector("#team");
+var person = team.querySelectorAll("section");
+for (let i = 0; i < person.length; i++) {
+    person[i].addEventListener('mouseover', function(){
+        enlightPerson(this);
+    });
+    person[i].addEventListener('mouseout', function () {
+        endarkPerson(this)
+    });
+}
+
+function enlightPerson(elmt) {
+    var resume = elmt.querySelector(".resume");
+    resume.classList.add("lightResume")
+}
+
+function endarkPerson(elmt) {
+    var resume = elmt.querySelector(".resume");
+    resume.classList.remove("lightResume")
+}
